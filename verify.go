756e415a36f4f3d489d6bc044051245728d39938
arvitaly <arvitaly@list.ru>
2016-01-27 04:22:17 +0300

    First version

---
+++ verify.go
@@ -0,0 +1,116 @@
+package verify
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	crand "crypto/rand"
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/hex"
+	"io"
+	"math/rand"
+	"net/url"
+	"strings"
+	"time"
+)
+
+func Create(str string, secret string, hash string) string {
+	return url.QueryEscape(base64.StdEncoding.EncodeToString([]byte(encode(leftPad2Len(str, "~", 16), secret)))) + "." + sha1get(hash+secret)
+}
+
+func Parse(code string, secret string) (id string, keyHash string, err error) {
+	var hash = code[len(code)-40:]
+	var crypt = code[:len(code)-41]
+	b2, err := url.QueryUnescape(crypt)
+	if err != nil {
+		return
+	}
+
+	b, err := base64.StdEncoding.DecodeString(b2)
+	if err != nil {
+		return
+	}
+	return strings.TrimLeft(decode([]byte(b), secret), "~"), hash, nil
+}
+
+func decode(ciphertext []byte, k string) string {
+	key := []byte(k)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := ciphertext[:aes.BlockSize]
+	ciphertext = ciphertext[aes.BlockSize:]
+	if len(ciphertext)%aes.BlockSize != 0 {
+		panic("ciphertext is not a multiple of the block size")
+	}
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(ciphertext, ciphertext)
+
+	return string(ciphertext)
+}
+
+func leftPad2Len(s string, padStr string, overallLen int) string {
+	var padCountInt int
+	padCountInt = 1 + ((overallLen - len(padStr)) / len(padStr))
+	var retStr = strings.Repeat(padStr, padCountInt) + s
+	return retStr[(len(retStr) - overallLen):]
+}
+
+func encode(str string, keys string) string {
+	key := []byte(keys)
+	plaintext := []byte(str)
+
+	// CBC mode works on blocks so plaintexts may need to be padded to the
+	// next whole block. For an example of such padding, see
+	// https://tools.ietf.org/html/rfc5246#section-6.2.3.2. Here we'll
+	// assume that the plaintext is already of the correct length.
+	if len(plaintext)%aes.BlockSize != 0 {
+		panic("plaintext is not a multiple of the block size")
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(crand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
+
+	// It's important to remember that ciphertexts must be authenticated
+	// (i.e. by using crypto/hmac) as well as being encrypted in order to
+	// be secure.
+
+	return string(ciphertext)
+}
+
+func sha1get(s string) string {
+	h := sha1.New()
+	h.Write([]byte(s))
+	return hex.EncodeToString(h.Sum(nil))
+}
+
+func randomString(l int) string {
+	rand.Seed(time.Now().UTC().UnixNano())
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
---
+++ verify_test.go
@@ -0,0 +1,44 @@
+package verify
+
+import "testing"
+
+var secret = `ruiyh684i7ug87\4ruiyh684i7ug87\4`
+
+func TestCreate(t *testing.T) {
+	var startStr = "2317083"
+	var sourceHash = "f39675ac2cbc33c4fedc838343b2cb75abdb6d2c"
+	str := Create(startStr, secret, sourceHash)
+
+	dest, hash, _ := Parse(str, secret)
+	if dest != startStr {
+		t.Error("Invalid create or parse")
+	}
+	if hash != sha1get(sourceHash+secret) {
+		t.Error("Invalid source hash")
+	}
+}
+
+func TestParse(t *testing.T) {
+
+	var code = "ZBBJbvsNdC59GG74oeineJ7k3UunLeNAO6juTnlnOJY=.546d92bb2c600359dd9c04e48ad873fdfcadb8c3"
+	var str, keyHash, err = Parse(code, secret)
+
+	if str != "0000000002317083" {
+		t.Log(str, keyHash, err)
+		t.Error("Invalid str")
+	}
+	if keyHash != sha1get("f39675ac2cbc33c4fedc838343b2cb75abdb6d2c"+secret) {
+		t.Error("Invalid key hash")
+	}
+	code = "kL%2FHOfH0UFmKW0iDvgHLUe06oOQFua9jRlJk4yXFviI%3D.0f080043fa62997e42abf16208235b686b014715"
+	str, keyHash, err = Parse(code, secret)
+	if str != "0000000002317103" {
+		t.Error("Invalid str")
+	}
+	if keyHash != sha1get("4ee5de9c0b36427a07376d8b85e9f48874fb642c"+secret) {
+		t.Error("Invalid key hash")
+	}
+	if err != nil {
+		t.Error(err)
+	}
+}
